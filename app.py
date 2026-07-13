af4d05ed2bdecb12474e164724e491594a286a3b
MikeyOgada <mikeogada254@gmail.com>
2026-07-13 07:14:34 +0100

    Vuln example

--- app.py
+++ app.py
@@ -1,6 +1,1 @@
-# app.py
-
-AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
-AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-
-print("Application starting...")
+GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyzABCD"
